6b3d1204fef8aafdca974fcafcbfe56fc4dd8c2a
softservedata <wapgprs@list.ru>
2015-11-10 20:39:24 +0200

    add java file

---
+++ src/com/softserve/edu/Appl.java
@@ -0,0 +1,12 @@
+package com.softserve.edu;
+
+public final class Appl {
+
+    private Appl() {
+    }
+
+    public static void main(final String[] args) {
+        System.out.println("Hello");
+    }
+
+}
