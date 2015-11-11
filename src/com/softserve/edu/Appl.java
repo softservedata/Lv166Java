389ae36d87ea81d55fd2087064dd887f4febe8d2
Korolyuk92 <korolyuk92@gmail.com>
2015-11-11 15:37:17 +0200

    change #2

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -6,7 +6,7 @@ public final class Appl {
     }
 
     public static void main(final String[] args) {
-        System.out.println("Hello VI!");
+        System.out.println("Hello VI!VI!VI!");
     }
 
 }
