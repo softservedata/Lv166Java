a226fb2cc561e670ae5aea1766cc9f0b6a892c4f
Korolyuk92 <korolyuk92@gmail.com>
2015-11-11 15:56:25 +0200

    delete few lines

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -1,18 +1,8 @@
 package com.softserve.edu;
 
 public final class Appl {
-
     private Appl() {
     }
-
     public static void main(final String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello VI!VI!VI!");
-=======
-        System.out.println("Hello VI!");
-		System.out.println("My new line");
-		
->>>>>>> 8c415b3334b73e365d7a069825f2acd406f8578d
     }
-
 }
