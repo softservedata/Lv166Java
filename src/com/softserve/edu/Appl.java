bdcf598df186dc1a76b2cfd201dccbee72baab64
Korolyuk92 <korolyuk92@gmail.com>
2015-11-16 00:34:33 +0200

    delete last line

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -7,6 +7,5 @@ public final class Appl {
         System.out.println("Hello VI!VI!VI!");   
 		System.out.println("My new line");
 		System.out.println("One more line");
-		System.out.println(“Hello Andrew”);
     }
 }
