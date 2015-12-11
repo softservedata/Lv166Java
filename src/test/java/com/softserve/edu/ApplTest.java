d345f335433f31a0f62b5ebe880351295cb98e4f
softservedata <wapgprs@list.ru>
2015-12-11 12:50:32 +0200

    Start Project

---
+++ src/test/java/com/softserve/edu/ApplTest.java
@@ -0,0 +1,14 @@
+package com.softserve.edu;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ApplTest {
+
+	@Test
+	public void testApp() {
+		Assert.assertTrue(true);
+		System.out.println("testApp() done");
+	}
+
+}
